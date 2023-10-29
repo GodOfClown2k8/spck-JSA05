720c9b23f66e8483d667b87745ab9fae78d50b19
GodOfClown2k8 <vonguyenminhkhoi1007@gmail.com>
2023-10-29 10:27:38 +0700

    commit

--- mainweb.js
+++ mainweb.js
@@ -62,3 +62,60 @@ function getTimeRemaining(endtime) {
   
   var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
   initializeClock('clockdiv', deadline);
+
+  let accounts =[];
+function register() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username && password) {
+      const existingAccount = accounts.find(account => account.username === username);
+
+      if (existingAccount) {
+        alert('Username đã tồn tại. Vui lòng chọn username khác.');
+        } else {
+            const account = { username, password };
+            accounts.push(account);
+            localStorage.setItem('accounts', JSON.stringify(accounts));
+            alert('Đăng ký thành công!');
+        }
+    } else {
+        alert('Vui lòng nhập username và password.');
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+  const loginStatusElement = document.getElementById('loginStatus');//!*!
+  
+  const isLoggedIn = true; //kiểm tra logic đăng nhập thực tế
+  
+  if (isLoggedIn) {
+      // Nếu đã đăng nhập, hiển thị tên người dùng
+      const username = ''; // 
+      loginStatusElement.textContent = `Xin chào, ${username}!`;
+  } else {
+      // Nếu chưa đăng nhập, hiển thị nút đăng nhập
+      const loginLink = document.createElement('a');
+      loginLink.href = '#';
+      loginLink.textContent = 'Đăng nhập';
+      loginStatusElement.appendChild(loginLink);
+  }
+});
+
+function login() {
+  const enteredUsername = document.getElementById('username').value;
+  const enteredPassword = document.getElementById('password').value;
+
+  if (enteredUsername && enteredPassword) {
+      const loggedInAccount = accounts.find(account => 
+          account.username === enteredUsername && account.password === enteredPassword
+      );
+
+      if (loggedInAccount) {
+          alert('Đăng nhập thành công!');
+      } else {
+          alert('Sai tên đăng nhập hoặc mật khẩu.');
+      }
+  } else {
+      alert('Vui lòng nhập tên đăng nhập và mật khẩu.');
+  }
+}
